a3acf253f182c9157d56cbaacef05c44e5d85424
Emma Hamilton <git@emmas.town>
2026-05-03 20:51:09 +1000

    disable pyn

--- modules/js.nix
+++ modules/js.nix
@@ -6,17 +6,17 @@
 }:
 {
   home.packages = with pkgs; [
-    (rustPlatform.buildRustPackage rec {
-      pname = "pyn";
-      version = "0.6.1";
+    # (rustPlatform.buildRustPackage rec {
+    #   pname = "pyn";
+    #   version = "0.6.1";
 
-      src = fetchCrate {
-        inherit pname version;
-        sha256 = "sha256-6Y3YLj/I26R92ZFH7punC4YHbPyDd0l8UdJ4nj+JyjM=";
-      };
-      doCheck = false;
-      cargoHash = "sha256-KwCn2oK96sMdTFB8wq+lUlWF/uQ1wgqbWWHHSfdBikI=";
-    })
+    #   src = fetchCrate {
+    #     inherit pname version;
+    #     sha256 = "sha256-6Y3YLj/I26R92ZFH7punC4YHbPyDd0l8UdJ4nj+JyjM=";
+    #   };
+    #   doCheck = false;
+    #   cargoHash = "sha256-KwCn2oK96sMdTFB8wq+lUlWF/uQ1wgqbWWHHSfdBikI=";
+    # })
     proto
   ];
   programs.zsh = {
